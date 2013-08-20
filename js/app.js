49a324fe85f9f04d5b4cf7a8553738dcbbb7635e
Kyle Millns <kyle.millns@gmail.com>
2013-08-20 09:46:44 -0500

    Day one of training

--- js/app.js
+++ js/app.js
@@ -1,6 +1,40 @@
 (function() {
-"use strict";
+	"use strict";
 
-window.App = Ember.Application.create();
+	window.App = Ember.Application.create();
 
+	window.App.Router.map(function () {
+		this.resource('album', {
+			path: '/album/:album_id'
+		});
+	});
+
+	window.App.IndexRoute = Ember.Route.extend({
+		model: function () {
+			return App.ALBUM_FIXTURES;
+		}
+	});
+
+	window.App.AlbumRoute = Ember.Route.extend({
+		model: function (params) {
+			return App.ALBUM_FIXTURES.findProperty('id', params.album_id);
+		}
+	});
+
+	Ember.Handlebars.helper('format-duration', function(value, options) {
+		var seconds = parseInt(value, 10);
+		var minutes = Math.floor(seconds/60);
+
+		seconds = seconds - (minutes * 60);
+
+		var prettyTime = minutes + ':';
+
+		if (seconds < 10) {
+			prettyTime = prettyTime + '0';
+		}
+
+		prettyTime = prettyTime + seconds;
+
+		return prettyTime;
+	});
 })();
